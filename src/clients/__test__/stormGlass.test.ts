de958ed548f1d8e8a0177d1d2f5cc1df4fc7063c
Anderson Ferreira <anderson@pop-os.localdomain>
2024-01-16 09:05:26 -0300

    test: melhorando o teste mockado da api de stormGlass

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -1,5 +1,7 @@
 import { StormGlass } from '@src/clients/StormGlass';
 import axios from 'axios';
+import stormGlassWeather3HoursFixture from '@test/fixtures/stormglass_weather_3.json';
+import stormGlassNormalized3HoursFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
 
 jest.mock('axios');
 
@@ -8,10 +10,10 @@ describe('StormGlass client', () => {
         const lat = -33.792726;
         const lng = 151.289824;
 
-        axios.get = jest.fn().mockResolvedValue({});
+        axios.get = jest.fn().mockResolvedValue(stormGlassWeather3HoursFixture);
 
         const stormGlass = new StormGlass(axios);
         const response = await stormGlass.fetchPoints(lat, lng);
-        expect(response).toEqual({});
-    })
-})
+        expect(response).toEqual(stormGlassNormalized3HoursFixture);
+    });
+});
